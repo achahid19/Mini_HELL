656940a31fe60e2f9b8a8c32c1b9a4e7195895b4
achahid19 <anasks1999@gmail.com>
2024-06-03 16:52:25 +0100

    expander: leanks clean up

--- sources/hellGate.c
+++ sources/hellGate.c
@@ -46,11 +46,11 @@ int	main(int ac, char **av, char **envp)
 		user_input = readline("kssh$ ");
 		if (user_input == NULL) // for (ctrl + d)
 			exit (1);
-		if (strcmp(user_input,"\0") != 0) // for history
+		if (ft_strncmp(user_input, "\0", 1) != 0) // for history
 			add_history(user_input);
 		tokens_list = lexer(user_input);
 		tokens_expander(tokens_list, envp);
-		check_tokens(tokens_list);
+		//check_tokens(tokens_list);
 		// exit(1);
 		parser_tokens(tokens_list);
 		// build_in(tokens_list);
