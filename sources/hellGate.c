2f961477d619fdcce12f357d0cd9df82c221e658
achahid19 <anasks1999@gmail.com>
2024-06-04 07:59:09 +0100

    update repo

--- sources/hellGate.c
+++ sources/hellGate.c
@@ -45,16 +45,16 @@ int	main(int ac, char **av, char **envp)
 	{
 		user_input = readline("kssh$ ");
 		if (user_input == NULL) // for (ctrl + d)
-			exit (1);
+			exit(EXIT_SUCCESS);
 		if (ft_strncmp(user_input, "\0", 1) != 0) // for history
 			add_history(user_input);
 		tokens_list = lexer(user_input);
 		tokens_expander(tokens_list, envp);
 		//check_tokens(tokens_list);
-		// exit(1);
 		parser_tokens(tokens_list);
 		// build_in(tokens_list);
 		free_tokens(tokens_list);
 		free(user_input);
 	}
+	return (EXIT_SUCCESS);
 }
