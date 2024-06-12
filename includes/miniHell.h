8c7d6b3efa949bbd6373fa63c23f1606e6361cf6
achahid19 <anasks1999@gmail.com>
2024-06-12 17:59:04 +0100

    update header file

--- includes/miniHell.h
+++ includes/miniHell.h
@@ -170,6 +170,7 @@ token_ptr	get_next_pipe(token_ptr tokens_list);
 /* syntax_builder */
 void		syntax_algo(token_ptr tokens_list);
 int			check_pipes_num(token_ptr tokens_list);
+ void	check_no_cmd(token_ptr tokens_list);
 
 /* tokens checker (printed on terminal) */
 void		check_tokens(token_ptr print_tk);
