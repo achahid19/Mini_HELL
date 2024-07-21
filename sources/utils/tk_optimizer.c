7b867fdf40f383528af8fbb10f6ee6f067b0938f
achahid19 <anasks1999@gmail.com>
2024-07-21 14:28:49 +0100

    Fix: Split words in optimizer
    Splits words in optimizer to account for whitespace after pipes.

--- sources/utils/tk_optimizer.c
+++ sources/utils/tk_optimizer.c
@@ -20,94 +20,6 @@ void		special_chars_refactor(token_ptr tokens_list);
 t_bool		special_chars_finder(token_ptr *tokens_list, token_ptr node_add,
 				int type);
 
-
-/**
- * pipe_node_remover -
- */
-void	pipe_node_remover(token_ptr *last)
-{
-	token_ptr	to_free;
-
-	to_free = (*last);
-	free((*last)->token);
-	(*last) = (*last)->previous;
-	if ((*last))
-		(*last)->next = NULL;
-	free(to_free);
-}
-
-/**
- * space_last_skip -
- */
-void	space_last_skip(token_ptr *last)
-{
-	while ((*last)->token_type == whitespace_token)
-	{
-		(*last) = (*last)->previous;
-		if ((*last) == NULL)
-			return ;
-	}
-}
-
-/**
- * pipe_order_check -
- */
-void	pipe_order_check(token_ptr *tokens_list)
-{
-	token_ptr	last;
-	token_ptr	begin;
-
-	if (*tokens_list == NULL)
-		return ;
-	last = find_last_node((*tokens_list));
-	while (last->token_type == pipe_token)
-	{
-		if (last->previous == NULL)
-			*tokens_list = NULL;
-		pipe_node_remover(&last);
-		if (last == NULL)
-			break ;
-	}
-}
-
-/**
- * rightred_order_check -
- */
-/* t_bool	rightred_order_check(token_ptr *tokens_list)
-{	
-	token_ptr	tmp;
-	t_bool		filename;
-
-	tmp = (*tokens_list);
-	filename = false;
-	while (tmp)
-	{
-		if (tmp->token_type == rightred_token
-			|| tmp->token_type == append_token)
-		{
-			while (tmp)
-			{
-				if ((tmp->token_type == rightred_token
-					|| tmp->token_type == append_token)
-					&& (ft_strncmp(tmp->token, ">", tmp->token_length) == 0
-					|| ft_strncmp(tmp->token, ">>", tmp->token_length) == 0))
-				{ */
-					/* if (open_output_fds(*tokens_list) == false)
-						return (false); */
-/* 					print_error("kssh: ambigious redirect\n");
-					return (false);
-				}
-				if (tmp->token_type == rightred_token
-					|| tmp->token_type == append_token)
-					filename = true;
-			}
-			break ;
-		}
-		tmp = tmp->next;
-	}
-	return (true);
-} */
-
 /**
  * tokens_list_optimizer -
  */
@@ -122,6 +34,7 @@ void	tokens_list_optimizer(token_ptr *tokens_list)
 	previous = NULL;
 	/* blank_nodes_remover(tokens_list, free_node, previous);
 	tokens_order(*tokens_list); */
+	split_words(tokens_list);
 	pipe_order_check(tokens_list);
 	/* if (rightred_order_check(tokens_list) == false)
 		return (false); */
